c6e09d39febb9bf69cc3b44da7bba0e177883da9
fredisanmar <40606800+fredisanmar@users.noreply.github.com>
2019-01-14 18:57:40 +0100

    Update node-analizer-v2.sh

--- node-analizer-v2.sh
+++ node-analizer-v2.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 apt-get install prips -y> /dev/null
 read -p '¿qué red quieres analizar?: ' network
@@ -6,16 +7,17 @@ prips $network>/tmp/ip-range.txt
 
 ##############################################################################
 
+
 node_status()
 {
-	if ping -c 2 $1 &> /dev/null
-	then
-		sleep 2
-		echo -e  "$host \e[1;32m[Up]\e[1;37m"
-		nslookup $host
-	else
-		echo -e "$host \e[0;31m[Down]\e[1;37m"
-	fi
+        if ping -c 1 $1 &> /dev/null
+        then
+                sleep 3
+                echo -e  "$host \e[1;32m[Up]\e[1;37m"
+                nslookup $host
+        else
+                echo -e "$host \e[0;31m[Down]\e[1;37m"
+        fi
 }
 
 ##############################################################################
@@ -23,5 +25,7 @@ node_status()
 for host in $(cat /tmp/ip-range.txt)
 do
 node_status $host & disown
-sleep 1
+sleep 0.2
 done
+sleep 3.5
+
